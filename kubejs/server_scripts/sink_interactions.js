64e89a6b062e0008210019c97931ad76102e6cbb
Madison <29849312+MythicalMontie@users.noreply.github.com>
2026-01-16 15:34:21 -0600

    update to include canteens as well

--- kubejs/server_scripts/sink_interactions.js
+++ kubejs/server_scripts/sink_interactions.js
@@ -29,6 +29,59 @@ BlockEvents.rightClicked(event => {
         event.cancel()
     }
     }
+ if (event.item.id == 'legendarysurvivaloverhaul:canteen' && kitchen_sinks.includes(event.block.id)) {
+    if (event.block.properties.filled == 'true') {
+        let nbt = event.item.nbt
+        let currentCapacity = 0
+        if (nbt != null && nbt.contains('legendarysurvivaloverhaul:HydrationCapacity')) {
+            currentCapacity = Number(nbt.get('legendarysurvivaloverhaul:HydrationCapacity'))
+        }
+
+        if (currentCapacity <10) {
+            event.block.set(event.block.id, {filled: "false", facing: event.block.properties.facing})
+            event.player.swing()
+            if (nbt == null) {
+                event.item.nbt = {}
+            }
+            event.item.nbt.putInt('legendarysurvivaloverhaul:HydrationCapacity', 10)
+            event.item.nbt.putString('legendarysurvivaloverhaul:HydrationPurity', "purified")
+            event.cancel()
+        }
+
+    }
+    else {
+        event.player.displayClientMessage("The sink is empty!", true)
+        event.cancel()
+    }
+    
+}
+ if (event.item.id == 'legendarysurvivaloverhaul:large_canteen' && kitchen_sinks.includes(event.block.id)) {
+    if (event.block.properties.filled == 'true') {
+        let nbt = event.item.nbt
+        let currentCapacity = 0
+        if (nbt != null && nbt.contains('legendarysurvivaloverhaul:HydrationCapacity')) {
+            currentCapacity = Number(nbt.get('legendarysurvivaloverhaul:HydrationCapacity'))
+        }
+
+        if (currentCapacity <20) {
+            event.block.set(event.block.id, {filled: "false", facing: event.block.properties.facing})
+            event.player.swing()
+            if (nbt == null) {
+                event.item.nbt = {}
+            }
+            event.item.nbt.putInt('legendarysurvivaloverhaul:HydrationCapacity', 20)
+            event.item.nbt.putString('legendarysurvivaloverhaul:HydrationPurity', "purified")
+            event.cancel()
+        }
+
+    }
+    else {
+        event.player.displayClientMessage("The sink is empty!", true)
+        event.cancel()
+    }
+    
+}
+
 })
 
 
